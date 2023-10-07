44db27e71fe9884cf56d3595ce39c5ec7ddbe779
Berat Cevik <beratbarancevik@gmail.com>
2023-10-07 17:22:04 +0100

    Add Firebase

--- UCLSeminar/Services/Stocks/StocksService.swift
+++ UCLSeminar/Services/Stocks/StocksService.swift
@@ -5,18 +5,26 @@
 //  Created by Berat Cevik on 07/10/2023.
 //
 
+import FirebaseFirestore
+
 final class StocksService: StockServiceProtocol {
 
-    init() {
+    private let db: Firestore
 
+    init(db: Firestore = Firestore.firestore()) {
+        self.db = db
     }
 
     func getStocks() {
+        db.collection("stocks").getDocuments { snapshot, error in
 
+        }
     }
     
     func getStockDetails(stockID: String) {
+        db.collection("stocks").document(stockID).getDocument { snapshot, error in
 
+        }
     }
 
 }
--- UCLSeminar/Supporting Files/AppDelegate.swift
+++ UCLSeminar/Supporting Files/AppDelegate.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
+import FirebaseCore
+
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
